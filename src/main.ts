397fde6f8daa886b65957c314841a55fa6f69dc4
JavierDavidCampoSuarezCampus <campo.javier1502@gmail.com>
2023-05-10 21:49:31 -0500

    feat: :sparkles: Taller 01
    Ejercicio 01

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
 /* TALLER 01 */
+/* ///////////////////////////////////////////////////////////// */
+/* 
+1.
+a.
+Declarar variables llamadas 'continente', 'país',
+'Departamento', 'ciudad', 'barrio' y 'dirección', y asigne sus
+valores de cantidad de habitantes, de acuerdo con su contexto
+real en su propio país.
+b.
+Registre sus valores en la consola.
+*/
+
+let 
+continente:string,
+pais:string,
+departamento:string,
+ciudad:string,
+barrio:string,
+direccion:string;
+
+continente = 'america';
+pais = 'colombia';
+departamento = 'santander';
+ciudad = 'bucaramanga';
+barrio = 'san Alonso';
+direccion = 'calle 21 #28-25';
+
+console.log(`
+    Estoy vivo en el continente de ${continente}, en\n
+    un pais llamado ${pais}, en donde estoy ubicado en el\n
+    departamento de ${departamento} dentro de la ciudad de ${ciudad},
+    en el barrio ${barrio} con direccion ${direccion}.
+`);
