40ca6a3d9717f18e52760d5825799a63a9b7da63
JavierDavidCampoSuarezCampus <campo.javier1502@gmail.com>
2023-05-10 21:53:07 -0500

    feat:

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,3 @@
 "use strict";
-var continente, pais, departamento, ciudad, barrio, direccion;
-continente = 'america';
-pais = 'colombia';
-departamento = 'santander';
-ciudad = 'bucaramanga';
-barrio = 'san Alonso';
-direccion = 'calle 21 #28-25';
-console.log("\n    Estoy vivo en el continente de ".concat(continente, ", en\n\n    un pais llamado ").concat(pais, ", en donde estoy ubicado en el\n\n    departamento de ").concat(departamento, " dentro de la ciudad de ").concat(ciudad, ",\n    en el barrio ").concat(barrio, " con direccion ").concat(direccion, ".\n"));
+var esTercerMundista = true, idioma = 'Español';
+console.log("\n    mi pais ".concat(esTercerMundista ? 'es' : 'no es', " Tercer Mundista,\n    ademas hablamos ").concat(idioma, "\n"));
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,22 @@
 /* TALLER 01 */
 /* ///////////////////////////////////////////////////////////// */
 /* 
-1.
+2.
 a.
-Declarar variables llamadas 'continente', 'país',
-'Departamento', 'ciudad', 'barrio' y 'dirección', y asigne sus
-valores de cantidad de habitantes, de acuerdo con su contexto
-real en su propio país.
+Declare una variable llamada 'esTercerMundista' y
+establezca su valor de acuerdo con su país. La variable debe
+contener un valor booleano. También declarar una variable
+'idioma', pero no le asigne ningún valor todavía.
 b.
-Registre sus valores en la consola.
+Registrar los valores de 'esTercerMundista',
+'continente', 'país' e 'idioma'
 */
 
 let 
-continente:string,
-pais:string,
-departamento:string,
-ciudad:string,
-barrio:string,
-direccion:string;
-
-continente = 'america';
-pais = 'colombia';
-departamento = 'santander';
-ciudad = 'bucaramanga';
-barrio = 'san Alonso';
-direccion = 'calle 21 #28-25';
+esTercerMundista:boolean = true,
+idioma:string = 'Español';
 
 console.log(`
-    Estoy vivo en el continente de ${continente}, en\n
-    un pais llamado ${pais}, en donde estoy ubicado en el\n
-    departamento de ${departamento} dentro de la ciudad de ${ciudad},
-    en el barrio ${barrio} con direccion ${direccion}.
+    mi pais ${esTercerMundista?'es':'no es'} Tercer Mundista,
+    ademas hablamos ${idioma}
 `);
