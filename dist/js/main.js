717e4176f17849bd91d98243a497f927f9292c7e
JavierDavidCampoSuarezCampus <campo.javier1502@gmail.com>
2023-05-12 07:14:25 -0500

    feat: :sparkles: TALLER 03 EJERCICIO 01

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,38 +1,114 @@
 "use strict";
-function campturarNumeros() {
-    let nuevoNumero, numeros = [];
-    while (nuevoNumero = parseInt(prompt('INGRESA UN NUEVO VALOR A LA SUMATORIA'))) {
-        numeros.unshift(nuevoNumero);
+let Bucaramanga = {
+    nombre: 'Bucaramanga',
+    direccion: 'vnsdkfjvndjf',
+    telefono: 34234898
+}, bogota = {
+    nombre: 'Bogota',
+    direccion: 'fbgnjkg78',
+    telefono: 988549
+}, medellin = {
+    nombre: 'Medellin',
+    direccion: 'nbjfknbj',
+    telefono: 540869450
+}, mexico = {
+    nombre: 'Ciudad de Mexico',
+    direccion: 'vbdhfjbvdhj',
+    telefono: 7348957
+}, python = {
+    nombre: 'PYTHON',
+    tieneSanbox: false,
+    esRemota: false
+}, javascript = {
+    nombre: 'JAVASCRIPT',
+    tieneSanbox: true,
+    esRemota: false
+}, php = {
+    nombre: 'PHP',
+    tieneSanbox: false,
+    esRemota: true
+}, typeScript = {
+    nombre: 'TYPESCRIPT',
+    tieneSanbox: true,
+    esRemota: true
+}, roadmap = {
+    nroCreditos: 3000,
+    nroAsignaturas: 4,
+    year: '2023'
+}, apolo = {
+    nombre: 'Apolo',
+    piso: 'PRINCIPAL',
+    nro: '6'
+}, artemise = {
+    nombre: 'artemise',
+    piso: 'PRINCIPAL',
+    nro: '6'
+}, spotnick = {
+    nombre: 'spotnick',
+    piso: 'PRINCIPAL',
+    nro: '6'
+}, ser = {
+    nombre: 'SER',
+    piso: 'AUDITORIO',
+    nro: '6'
+}, ingles = {
+    nombre: 'SER',
+    piso: 'AUDITORIO',
+    nro: '6'
+}, basico = {
+    tecnologia: python,
+    esElectiva: false
+}, medio = {
+    prerequisito: [python, javascript],
+    tecnologia: php,
+    esElectiva: false
+}, avanzado = {
+    prerequisito: [python, javascript, php],
+    tecnologia: typeScript,
+    esElectiva: true
+}, m2 = {
+    dia: '23-12-2024',
+    hora: '4:00 - 10:00',
+    salones: [ingles, ser, spotnick]
+}, j2 = {
+    dia: '19-04-2023',
+    hora: '4:00 - 10:00',
+    salones: [ingles, ser, artemise]
+}, d2 = {
+    dia: '07-01-2023',
+    hora: '7:00 - 16:00',
+    salones: [ingles, ser, apolo]
+}, juan = {
+    nombre: 'juan david',
+    telefono: 9878934,
+    email: 'dnvjdknfv@vfmkdl.cds',
+    teams: [m2, j2],
+    asignaturas: [php, javascript]
+}, paco = {
+    nombre: 'paco adrian',
+    telefono: 765312123,
+    email: 'fdsjng@vdl.cds',
+    teams: [m2, d2],
+    asignaturas: [python, php, typeScript]
+}, pedro = {
+    barrio: 'csd',
+    transporte: 'bus',
+    nivelActual: avanzado,
+    nombre: 'pedro de la mar',
+    telefono: 2343290,
+    email: 'nbjkbg.nj@fvmjd.vfd',
+    teams: [m2],
+    horarios: {
+        ingles: ingles,
+        ser: ser
     }
-    return numeros;
-}
-function sumatoriaDeLosValores(...numeros) {
-    return numeros.reduce((numeroPrevio, numeroSiguiente) => {
-        return numeroPrevio + numeroSiguiente;
-    });
-}
-function mayorNumero(...numeros) {
-    return numeros.reduce((numeroPrevio, numeroSiguiete) => {
-        if (numeroPrevio < numeroSiguiete)
-            return numeroSiguiete;
-        return numeroPrevio;
-    });
-}
-function menorNumero(...numeros) {
-    return numeros.reduce((numeroPrevio, numeroSiguiete) => {
-        if (numeroPrevio > numeroSiguiete)
-            return numeroSiguiete;
-        return numeroPrevio;
-    });
-}
-function promedioNumeros(numeros) {
-    return sumatoriaDeLosValores(...numeros) / numeros.length;
-}
-const Numeros = campturarNumeros();
-console.log(`
-LA SUMATORIA DE LOS VALORES ES DE ${sumatoriaDeLosValores(...Numeros)}.\n
-EL VALOR DEL PROMEDIO ES ${promedioNumeros(Numeros)}.\n
-CUANTOS VALORES FUERON DIGITADOS ${Numeros.length}.\n
-EL MAYOR NUMERO ES ${mayorNumero(...Numeros)}.\n
-EL MENOR NUMERO ES ${menorNumero(...Numeros)}.\n
-`);
+}, campus = {
+    trainers: [juan, paco],
+    campers: [pedro],
+    niveles: [avanzado, basico, medio],
+    sedes: [bogota, medellin, Bucaramanga, mexico],
+    tecnologias: [php, python, javascript, typeScript],
+    teams: [m2, j2, d2],
+    roadMap: roadmap
+};
+console.log(campus);
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,248 @@
-/* TALLER 02 */
+/* TALLER 03 */
 /* ///////////////////////////////////////////////////////////// */
 /* 
-10. Desarrolle un programa cíclico que capture un dato
-numérico cada vez, y los vaya acumulando. El programa se
-detiene cuando el usuario digita un cero. El programa debe
-mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
-PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
-VALOR Y MENOR VALOR.
+1. Construir un objeto literal "campus" que gestione
+la info(PROPIEDADES) de Campus, trainers, campers, niveles,
+tecnologías, teams y roadMap
+1.1. De campus administrar los datos de contacto de las sedes en
+Bucaramanga, Bogotá, Medellín y México
+1.2. De los trainers y campers, su nombre, sus teléfonos, teams
+(horarios de las teams=> día, hora y salones (nro y piso), y el
+email, y de los campers también horarios de inglés y ser.
+1.3. De los campers, también gestionar su nivel actual, como su
+barrio y medio de transporte
+1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
+es electiva u obligatoria
+1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas
+////////    ////////////////////777
+
+2. Consultas: Usando Destructuring,
+2.1 De los trainers, reportar si la asignatura (tecnología) es
+remota o presencial y de los campers el nombre de salón.
+2.2 El teléfono de la sede de Medellín y la dirección de la
+sede de Bucaramanga
+2.3 De la asignatura (tecnología) si tiene sandbox o no
+////////////////////////////
+
+3. Consultas: Usando sintaxis de punto.
+3.1 Reportar, prerequisito de la asignatura (tecnología) y nro de
+créditos del roadmap
+3.2 Agregar mas objetos con mas objetos anidados de manera libre
+(por lo menos 7)
 */
-function campturarNumeros():number[]{
-    let 
-    nuevoNumero:number,
-    numeros:number[] = [];
+
+type Sede = {
+    nombre:string,
+    telefono:number,
+    direccion:string
+}
+
+type Roadmap = {
+    nroCreditos:number,
+    nroAsignaturas:number
+    year:string,
     
-    while (nuevoNumero = parseInt(prompt('INGRESA UN NUEVO VALOR A LA SUMATORIA') as string)) {
-        numeros.unshift(nuevoNumero);
-    }    
-    return numeros    
 }
 
-function sumatoriaDeLosValores(...numeros:number[]){
-    return numeros.reduce((numeroPrevio:number,numeroSiguiente:number):number=>{
-        return numeroPrevio + numeroSiguiente;
-    })
+type Tecnologia = {
+    nombre:string,
+    tieneSanbox:boolean,
+    esRemota:boolean    
 }
 
-function mayorNumero(...numeros:number[] ){
-    return numeros.reduce((numeroPrevio:number,numeroSiguiete:number):number=>{
-        if(numeroPrevio<numeroSiguiete)return numeroSiguiete;
-        return numeroPrevio;
-    })
-}
-function  menorNumero(...numeros:number[] ){
-    return numeros.reduce((numeroPrevio:number,numeroSiguiete:number):number=>{
-        if(numeroPrevio>numeroSiguiete)return numeroSiguiete;
-        return numeroPrevio;
-    })
-}
-function promedioNumeros(numeros:number[]){
-    return sumatoriaDeLosValores(...numeros)/numeros.length;
+type Salon = {
+    nombre:string,
+    nro: string,
+    piso:string
 }
 
-const Numeros = campturarNumeros();
+type Nivel = {
+    prerequisito?:Tecnologia[],
+    tecnologia:Tecnologia,
+    esElectiva:boolean
+}
 
-console.log(`
-LA SUMATORIA DE LOS VALORES ES DE ${sumatoriaDeLosValores(...Numeros)}.\n
-EL VALOR DEL PROMEDIO ES ${promedioNumeros(Numeros)}.\n
-CUANTOS VALORES FUERON DIGITADOS ${Numeros.length}.\n
-EL MAYOR NUMERO ES ${mayorNumero(...Numeros)}.\n
-EL MENOR NUMERO ES ${menorNumero(...Numeros)}.\n
-`);
+type Team = {
+    dia:string,
+    hora:string,
+    salones:Salon[]
+}
+
+type Usuario = {
+    nombre:string,
+    telefono:number,
+    email:string,
+    teams:Team[]
+}
+
+type Camper = Usuario & {
+    barrio:string,
+    transporte:string,
+    nivelActual:Nivel
+    horarios:{
+        ingles:Salon,
+        ser:Salon
+    }
+}
+
+type Trainer = Usuario & {
+    asignaturas:Tecnologia[],    
+}
+
+
+type Campus = {
+    trainers:Trainer[],
+    campers:Camper[],
+    niveles:Nivel[],
+    sedes:Sede[],
+    tecnologias:Tecnologia[],
+    teams:Team[],
+    roadMap:Roadmap
+}
+
+/* CREAR SEDES */
+let 
+Bucaramanga:Sede = {
+    nombre: 'Bucaramanga',
+    direccion: 'vnsdkfjvndjf',
+    telefono:34234898
+},
+bogota:Sede = {
+    nombre: 'Bogota',
+    direccion: 'fbgnjkg78',
+    telefono:988549
+},
+medellin:Sede = {
+    nombre: 'Medellin',
+    direccion: 'nbjfknbj',
+    telefono:540869450
+},
+mexico:Sede = {
+    nombre:'Ciudad de Mexico',
+    direccion: 'vbdhfjbvdhj',
+    telefono:7348957
+},
+
+/* Tecnologia  */
+python:Tecnologia = {
+    nombre:'PYTHON',
+    tieneSanbox:false,
+    esRemota:false
+},
+javascript:Tecnologia = {
+    nombre:'JAVASCRIPT',
+    tieneSanbox:true,
+    esRemota:false
+},
+php:Tecnologia = {
+    nombre:'PHP',
+    tieneSanbox:false,
+    esRemota:true
+},
+typeScript:Tecnologia = {
+    nombre:'TYPESCRIPT',
+    tieneSanbox:true,
+    esRemota:true
+},
+/* Roadmap */
+roadmap:Roadmap = {
+    nroCreditos:3000,
+    nroAsignaturas:4,
+    year:'2023'
+},
+/* SALONES */
+apolo:Salon = {
+    nombre:'Apolo',
+    piso:'PRINCIPAL',
+    nro:'6'
+},
+artemise:Salon = {
+    nombre:'artemise',
+    piso:'PRINCIPAL',
+    nro:'6'
+},
+spotnick:Salon = {
+    nombre:'spotnick',
+    piso:'PRINCIPAL',
+    nro:'6'
+},
+ser:Salon = {
+    nombre:'SER',
+    piso:'AUDITORIO',
+    nro:'6'
+},
+ingles:Salon = {
+    nombre:'SER',
+    piso:'AUDITORIO',
+    nro:'6'
+},
+/* Nivel */
+basico:Nivel = {    
+    tecnologia:python,
+    esElectiva:false
+},
+medio:Nivel = {    
+    prerequisito:[python,javascript],
+    tecnologia:php,
+    esElectiva:false    
+},
+avanzado:Nivel = {    
+    prerequisito:[python,javascript,php],
+    tecnologia:typeScript,
+    esElectiva:true
+},
+/* Team */
+m2:Team = {
+    dia:'23-12-2024',
+    hora:'4:00 - 10:00',
+    salones: [ingles,ser,spotnick]
+},
+j2:Team = {
+    dia:'19-04-2023',
+    hora:'4:00 - 10:00',
+    salones: [ingles,ser,artemise]
+},
+d2:Team = {
+    dia:'07-01-2023',
+    hora:'7:00 - 16:00',
+    salones: [ingles,ser,apolo]
+},
+/* Usuario  */
+juan:Trainer = {
+    nombre:'juan david',
+    telefono: 9878934,
+    email:'dnvjdknfv@vfmkdl.cds',
+    teams:[m2,j2],
+    asignaturas:[php,javascript]
+},
+paco:Trainer = {
+    nombre:'paco adrian',
+    telefono: 765312123,
+    email:'fdsjng@vdl.cds',
+    teams:[m2,d2],
+    asignaturas:[python,php,typeScript]
+},
+pedro:Camper = {
+    barrio:'csd',
+    transporte:'bus',
+    nivelActual:avanzado,
+    nombre:'pedro de la mar',
+    telefono: 2343290,
+    email:'nbjkbg.nj@fvmjd.vfd',
+    teams:[m2],
+    horarios:{
+        ingles:ingles,
+        ser:ser
+    }
+},
+campus:Campus = {
+    trainers:[juan,paco],
+    campers:[pedro],
+    niveles:[avanzado,basico,medio],
+    sedes:[bogota,medellin,Bucaramanga,mexico],
+    tecnologias:[php,python,javascript,typeScript],
+    teams:[m2,j2,d2],
+    roadMap:roadmap
+};
+
+console.log(campus);
