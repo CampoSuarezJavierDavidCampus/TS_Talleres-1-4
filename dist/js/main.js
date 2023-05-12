67f851dc99285644cd7354b287968a25d44f6192
JavierDavidCampoSuarezCampus <campo.javier1502@gmail.com>
2023-05-11 22:20:51 -0500

    feat: :sparkles: TALLER 02 EJERCICIO 09

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,52 @@
 "use strict";
-let cuadrado = {
-    lados: 0,
-    primetro() {
-        return `EL PERIMETRO DEL CUADRADO ES ${this.lados * 4}`;
-    }
-}, rectangulo = {
-    lados: [0, 0],
-    primetro() {
-        return `EL PERIMETRO DEL RECTANGULO ES  ${2 * this.lados[0] + (2 * this.lados[1])}`;
-    }
-};
-cuadrado.lados = parseInt(prompt('INGRESA EL VALOR DEL LADO DEL CUADRADO\n=>'));
-alert(cuadrado.primetro());
-rectangulo.lados = [
-    parseInt(prompt('INGRESA LA ALTURA DEL RECTANGULO')),
-    parseInt(prompt('INGRESA LA BASE DEL RECTANGULO'))
-];
-alert(rectangulo.primetro());
+function crearAtleta() {
+    let nombre = prompt('INGRESE EL NOMBRE DE LA COMPETIDORA\n=>').trim().toUpperCase(), marcas = [], mejorMarca = 0;
+    for (let i = 0; i < 3; i++)
+        marcas.unshift(parseFloat(prompt(`INGRESA LA MARCA N°${i + 1} DE ${nombre}\n =>`)));
+    mejorMarca = mayorMarca(marcas);
+    if (!validarDatos(nombre, marcas))
+        return null;
+    return {
+        nombre: nombre,
+        marca: mejorMarca
+    };
+}
+function validarDatos(nombre, marcas) {
+    if (nombre.length == 0, marcas.filter((marca) => isNaN(marca)).length != 0)
+        return false;
+    return true;
+}
+function mayorMarca(marcas) {
+    return marcas.reduce((marcaAnterior, marcaSiguiente) => {
+        if (marcaAnterior < marcaSiguiente)
+            return marcaSiguiente;
+        return marcaAnterior;
+    });
+}
+function obtenerGanador(atletas) {
+    const AtletaGanador = atletas.reduce((atletaAnterior, atletaSiguiente) => {
+        if (atletaAnterior.marca < atletaSiguiente.marca)
+            return atletaSiguiente;
+        return atletaAnterior;
+    });
+    return `
+        LA ATLETA GANADORA FUE ${AtletaGanador.nombre} CON UN PUNTAJE DE ${AtletaGanador.marcaAnterior}M\n
+        ${AtletaGanador.marca > 15.50 ? 'ADEMAS GANO' : 'PERO NO GANO'} EL PREMIO DE 500 M POR ROMPER EL RECORD MUNDIAL
+    `;
+}
+function agregarAtletas() {
+    let atletas = [];
+    do {
+        const nuevaAtleta = crearAtleta();
+        if (nuevaAtleta == null)
+            continue;
+        else
+            atletas.unshift(nuevaAtleta);
+    } while (prompt(`DESEA ANADIR OTRA COMPETIDORA\nDIGITE "SI" PARA CONTINUAR.=>`).trim().toUpperCase() == 'SI');
+    return atletas;
+}
+const Atletas = agregarAtletas();
+if (Atletas.length == 0)
+    alert('NO HAY ATLETAS REGISTRADOS');
+else
+    alert(obtenerGanador(Atletas));
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,78 @@
 /* TALLER 02 */
 /* ///////////////////////////////////////////////////////////// */
 /* 
-8. Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo
+9. N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros.
 */
-type Cuadrilatero = {
-    lados:number[] | number,
-    primetro:()=>string
+
+type Atleta = {
+    nombre:string,
+    marca:number
 }
-let 
-    cuadrado:Cuadrilatero = {
-        lados: 0,
-        primetro(){                        
-            return `EL PERIMETRO DEL CUADRADO ES ${this.lados as number * 4}`;
-        } 
-    },
-    rectangulo:Cuadrilatero = {
-        lados: [0,0],
-        primetro(){            
-            return `EL PERIMETRO DEL RECTANGULO ES  ${(2 * (this.lados as number[])[0] as number ) + (2 * (this.lados as number[])[1 as number] )}`;
-        }
-    };    
 
-cuadrado.lados = parseInt(prompt('INGRESA EL VALOR DEL LADO DEL CUADRADO\n=>') as string)
-alert(cuadrado.primetro());
+function crearAtleta():null | Atleta {
+    let 
+        nombre = (prompt('INGRESE EL NOMBRE DE LA COMPETIDORA\n=>') as string).trim().toUpperCase(),
+        marcas:number[] = [],
+        mejorMarca = 0;
 
-rectangulo.lados = [
-    parseInt(prompt('INGRESA LA ALTURA DEL RECTANGULO') as string),
-    parseInt(prompt('INGRESA LA BASE DEL RECTANGULO') as string)
-];
+    for (let i = 0; i < 3; i++) 
+        marcas.unshift(parseFloat(prompt(`INGRESA LA MARCA N°${i+1} DE ${nombre}\n =>`) as string));
 
-alert(rectangulo.primetro());
+    mejorMarca = mayorMarca(marcas);
+
+    if(!validarDatos(nombre ,marcas)) return null;
+    return {
+        nombre: nombre,
+        marca:mejorMarca 
+    }
+}
+
+function validarDatos(nombre:string,marcas:number[]):boolean{    
+    if(nombre.length==0, marcas.filter((marca)=>isNaN(marca)).length != 0) return false;
+    return true    
+}
+
+function mayorMarca(marcas:number[]){
+    return marcas.reduce((marcaAnterior:number,marcaSiguiente:number):number=>{
+        if(marcaAnterior < marcaSiguiente) return marcaSiguiente;
+        return marcaAnterior;
+    })
+}
+
+function obtenerGanador(atletas:Atleta[]):string{
+    const AtletaGanador:Atleta =  atletas.reduce((atletaAnterior:Atleta,atletaSiguiente:Atleta)=>{
+        if(atletaAnterior.marca < atletaSiguiente.marca) return atletaSiguiente;
+        return atletaAnterior;        
+    });
+
+    return `
+        LA ATLETA GANADORA FUE ${AtletaGanador.nombre} CON UN PUNTAJE DE ${AtletaGanador}M\n
+        ${AtletaGanador.marca > 15.50?'ADEMAS GANO':'PERO NO GANO'} EL PREMIO DE 500 M POR ROMPER EL RECORD MUNDIAL
+    `;
+}
+
+function agregarAtletas():Atleta[]{
+    let atletas:Atleta[] = [];
+    do {        
+        const nuevaAtleta = crearAtleta();        
+
+        if(nuevaAtleta == null) continue;
+        else atletas.unshift(nuevaAtleta);
+        
+    } while ((prompt(`DESEA ANADIR OTRA COMPETIDORA\nDIGITE "SI" PARA CONTINUAR.=>`) as string).trim().toUpperCase() == 'SI');
+
+    return atletas;
+}
+
+const Atletas = agregarAtletas()
+
+
+if(Atletas.length == 0) alert('NO HAY ATLETAS REGISTRADOS')
+else alert(obtenerGanador(Atletas));
+
