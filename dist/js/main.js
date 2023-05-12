8a26385956b58aec72c2024d24c88d4c92be5ed4
JavierDavidCampoSuarezCampus <campo.javier1502@gmail.com>
2023-05-11 20:25:58 -0500

    feat: :sparkles: TALLER 02 EJERCICIO 06

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,50 @@
 "use strict";
-const Numero01 = parseInt(prompt('INGRESE EL PRIMER NUMERO')), Numero02 = parseInt(prompt('INGRESE EL SEGUNDO NUMERO'));
-Numero01 > Numero02
-    ? alert(`LA SUMA DE ${Numero01} CON ${Numero02} ES ${Numero01 + Numero02} Y SU DIFERENCIA ${Numero01 - Numero02}`)
-    : alert(`EL PRODUCTO DE ${Numero01} CON ${Numero02} ES ${Numero01 * Numero02} Y SU DIVICION ${Numero02 / Numero02}`);
+function obtenerEstudiantes() {
+    var _a, _b, _c;
+    let estudiantes = [];
+    do {
+        let nombreEstudiante = (_a = prompt('INGRESA EL NOMBRE DEL ESTUDIANTE')) === null || _a === void 0 ? void 0 : _a.trim().toLocaleUpperCase(), generoEstudiante = (_b = prompt('INGRESA EL GENERO DEL ESTUDIANTE\nM/F=>')) === null || _b === void 0 ? void 0 : _b.trim().toLocaleUpperCase(), notaEstudiante = parseFloat(prompt('INGRESA LA NOTA DEL ESTUDIANTE=>'));
+        if ((generoEstudiante !== 'M' && generoEstudiante !== 'F') || nombreEstudiante.length == 0)
+            continue;
+        const nuevoEstudiante = {
+            nombre: nombreEstudiante,
+            genero: generoEstudiante,
+            nota: notaEstudiante
+        };
+        estudiantes.unshift(nuevoEstudiante);
+    } while (((_c = prompt('DESEA AGREGAR ALGUN ESTUDAIANTE\nDIGITE "SI" PARA CONTINUAR.=>')) === null || _c === void 0 ? void 0 : _c.toUpperCase()) == 'SI');
+    return estudiantes;
+}
+function obtenerEstudianteMayorOMenorNota(estudiantes, solicitudDeLaMayorNota = true) {
+    let estudianteMayorNota = estudiantes.reduce((estudianteAnterior, estudianteSiguiente) => {
+        if (solicitudDeLaMayorNota)
+            return mayorNota(estudianteAnterior, estudianteSiguiente);
+        return menorNota(estudianteAnterior, estudianteSiguiente);
+    });
+    return `EL ESTUDIANTE ${estudianteMayorNota.nombre}
+     CON LA NOTA DE ${estudianteMayorNota.nota} POSEE LA ${solicitudDeLaMayorNota ? 'MAYOR' : 'MENOR'} NOTA`;
+}
+function mayorNota(estudianteAnterior, estudianteSiguiente) {
+    if (estudianteAnterior.nota < estudianteSiguiente.nota)
+        return estudianteSiguiente;
+    return estudianteAnterior;
+}
+function menorNota(estudianteAnterior, estudianteSiguiente) {
+    if (estudianteAnterior.nota > estudianteSiguiente.nota)
+        return estudianteSiguiente;
+    return estudianteAnterior;
+}
+function conteoDeGenerosDeLosEstudiantes(estudiantes) {
+    let contadorDeM = 0, contadorDeF = 0;
+    estudiantes.forEach((estudiante) => {
+        estudiante.genero == 'F' ? contadorDeF++ : contadorDeM++;
+    });
+    return `EL NUMERO DE ESTUDIANTES M:${contadorDeM} Y EL NUMERO DE ESTUDIANTES F:${contadorDeF}`;
+}
+let estudiantes = obtenerEstudiantes();
+if (estudiantes.length !== 0) {
+    console.log(obtenerEstudianteMayorOMenorNota(estudiantes) + '\n', obtenerEstudianteMayorOMenorNota(estudiantes, false) + '\n', conteoDeGenerosDeLosEstudiantes(estudiantes));
+}
+else {
+    console.log(`NO HAY ESTUDIANTES REGISTRADOS`);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,76 @@
 /* TALLER 02 */
 /* ///////////////////////////////////////////////////////////// */
 /* 
-5. Construir el algoritmo que lea por teclado dos números,
-si el primero es mayor al segundo informar su suma y
-diferencia, en caso contrario, informar el producto y la
-división del primero respecto al segundo.
-*/
-const
-Numero01:number = parseInt(prompt('INGRESE EL PRIMER NUMERO') as string),
-Numero02:number = parseInt(prompt('INGRESE EL SEGUNDO NUMERO') as string);
+6. Construir el algoritmo en Javascript para un programa
+para cualquier cantidad de estudiantes que lea el nombre,
+el sexo y la nota definitiva y halle al estudiante con la mayor
+nota y al estudiante con la menor nota y cuantos eran
+hombres y cuantos mujeres.
 
-Numero01 > Numero02 
-    ?   alert(`LA SUMA DE ${Numero01} CON ${Numero02} ES ${Numero01 + Numero02} Y SU DIFERENCIA ${Numero01 - Numero02}`)
-    :   alert(`EL PRODUCTO DE ${Numero01} CON ${Numero02} ES ${Numero01 * Numero02} Y SU DIVICION ${Numero02/Numero02}`);
-    
+*/
+type Estudiante = {
+    nombre:string,
+    genero: 'M'|'F',
+    nota:number
+}
+
+function obtenerEstudiantes():Estudiante[] {
+    let estudiantes:Estudiante[] = []
+    do {
+        let
+        nombreEstudiante = prompt('INGRESA EL NOMBRE DEL ESTUDIANTE')?.trim().toLocaleUpperCase() as string,
+        generoEstudiante = prompt('INGRESA EL GENERO DEL ESTUDIANTE\nM/F=>')?.trim().toLocaleUpperCase() as string,
+        notaEstudiante = parseFloat(prompt('INGRESA LA NOTA DEL ESTUDIANTE=>') as string);
+        if((generoEstudiante !== 'M' && generoEstudiante !== 'F') || nombreEstudiante.length == 0) continue;
+
+        const nuevoEstudiante:Estudiante = {
+            nombre: nombreEstudiante,        
+            genero: generoEstudiante,
+            nota: notaEstudiante
+        }
+
+        estudiantes.unshift(nuevoEstudiante);    
+    } while (prompt('DESEA AGREGAR ALGUN ESTUDAIANTE\nDIGITE "SI" PARA CONTINUAR.=>')?.toUpperCase() == 'SI');
+    return estudiantes;
+}
+
+function obtenerEstudianteMayorOMenorNota(estudiantes:Estudiante[],solicitudDeLaMayorNota:boolean = true):string{
+    let estudianteMayorNota = estudiantes.reduce((estudianteAnterior:Estudiante,estudianteSiguiente:Estudiante):Estudiante=>{
+        if(solicitudDeLaMayorNota)return mayorNota(estudianteAnterior,estudianteSiguiente);
+        return menorNota(estudianteAnterior,estudianteSiguiente);
+    });
+
+    return `EL ESTUDIANTE ${estudianteMayorNota.nombre}
+     CON LA NOTA DE ${estudianteMayorNota.nota} POSEE LA ${solicitudDeLaMayorNota?'MAYOR':'MENOR'} NOTA`;
+}
+
+function mayorNota(estudianteAnterior:Estudiante,estudianteSiguiente:Estudiante):Estudiante{
+    if(estudianteAnterior.nota < estudianteSiguiente.nota) return estudianteSiguiente;
+    return estudianteAnterior;
+}
+
+function menorNota(estudianteAnterior:Estudiante,estudianteSiguiente:Estudiante):Estudiante{
+    if(estudianteAnterior.nota > estudianteSiguiente.nota) return estudianteSiguiente;
+    return estudianteAnterior;
+}
+
+function conteoDeGenerosDeLosEstudiantes(estudiantes:Estudiante[]):string{
+    let
+        contadorDeM:number = 0,
+        contadorDeF:number = 0;
+    estudiantes.forEach((estudiante:Estudiante):void=>{
+        estudiante.genero == 'F'?contadorDeF++:contadorDeM++;
+    })
+    return `EL NUMERO DE ESTUDIANTES M:${contadorDeM} Y EL NUMERO DE ESTUDIANTES F:${contadorDeF}`;
+}
+
+let estudiantes = obtenerEstudiantes();
+if(estudiantes.length!==0){
+    console.log(
+        obtenerEstudianteMayorOMenorNota(estudiantes) + '\n',
+        obtenerEstudianteMayorOMenorNota(estudiantes,false) + '\n',
+        conteoDeGenerosDeLosEstudiantes(estudiantes)
+    );
+}else{
+    console.log(`NO HAY ESTUDIANTES REGISTRADOS`)
+}
